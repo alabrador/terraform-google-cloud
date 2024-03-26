eabccb4abbebd546b0ab3f39bd6a36b1f8bc27d7
Alexander Labrador <globehackers@gmail.com>
2024-03-26 23:09:42 +0100

    update

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 #Create a single Compute Engine instance
-resource "google_compute_instance" {
+resource "google_compute_instance" "default" {
   name         = "lupin3"
   machine_type = "f1-micro"
   zone         = "us-west1-a"
@@ -14,4 +14,12 @@ resource "google_compute_instance" {
   # Install Apache
   metadata_startup_script = "sudo apt-get update; sudo apt-get install -yq apache2 php; systemctl start apache2"
 
+  network_interface {
+    subnetwork = google_compute_subnetwork
+  }
+}
+
+// A variable for extracting the external IP address of the VM
+output "Web-server-URL" {
+ value = join("",["http://",google_compute_instance.network_interface.0.access_config.0.nat_ip,":80"])
 }
